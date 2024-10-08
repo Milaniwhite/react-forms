731c314774b2d1d07a506a9527052e230a7a2fca
Milani <milaniwhite12@gmail.com>
2024-10-07 22:00:29 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+import Authenticate from "./componets/Authenicate";
+import SignUpForm from "./componets/SignUpForm";
+import { useState } from "react";
+export default function App() {
+  const [token, setToken] = useState(null);
 
   return (
     <>
-     
+            
+      <SignUpForm token={token} setToken={setToken} />
+            
+      <Authenticate token={token} setToken={setToken} />
+          
     </>
-  )
+  );
 }
-
-export default App
